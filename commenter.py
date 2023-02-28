0319312ee4d3a5eb1e3221a9266283fae29b78a3
Erick <erick@maple.finance>
2023-02-28 00:32:18 -0500

    more to commneter

--- commenter.py
+++ commenter.py
@@ -4,21 +4,24 @@ import os
 for root, dirs, files in os.walk(".", topdown=False):
    for name in files:
       if name.endswith('talon'):
-        if os.path.join(root, name) == "./settings.talon":
+        pathfull = os.path.join(root, name)
+        if pathfull == "./settings.talon" or "./cheatsheet/cheatsheet.talon":
            continue
 
-        print(os.path.join(root, name))
+        print(pathfull)
         # Open the file as read
-        f = open("text.txt", "r+")
+        f = open(pathfull, "r+")
         # Create an array to hold write data
         new_file = []
         # Loop the file line by line
         for line in f:
+            if line.strip() == "":
+               continue
             # Split A,B on , and use first position [0], aka A, then add to the new array
             commented_line = f'# + {line}'
             # Add
             new_file.append(commented_line)
         # Open the file as Write, loop the new array and write with a newline
-        with open("text.txt", "w+") as f:
+        with open(pathfull, "w+") as f:
             for i in new_file:
                 f.write(i+"\n")
