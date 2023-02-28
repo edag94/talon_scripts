978086a33958b3e33355f6e8d9db8db7ce6fa2a7
Erick <erick@maple.finance>
2023-02-28 00:33:53 -0500

    fix plus

--- commenter.py
+++ commenter.py
@@ -5,7 +5,7 @@ for root, dirs, files in os.walk(".", topdown=False):
    for name in files:
       if name.endswith('talon'):
         pathfull = os.path.join(root, name)
-        if pathfull == "./settings.talon" or "./cheatsheet/cheatsheet.talon":
+        if pathfull == "./settings.talon" or pathfull == "./cheatsheet/cheatsheet.talon":
            continue
 
         print(pathfull)
@@ -18,7 +18,7 @@ for root, dirs, files in os.walk(".", topdown=False):
             if line.strip() == "":
                continue
             # Split A,B on , and use first position [0], aka A, then add to the new array
-            commented_line = f'# + {line}'
+            commented_line = f'# {line}'
             # Add
             new_file.append(commented_line)
         # Open the file as Write, loop the new array and write with a newline
